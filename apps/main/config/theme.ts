519de290893cb522833b72e4ac98a51375f83648
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-01-24 23:10:34 +0200

    chore: update theme with new tokens

--- apps/main/config/theme.ts
+++ apps/main/config/theme.ts
@@ -1,34 +1,61 @@
 import { MantineThemeOverride } from '@mantine/core';
 
 const theme: MantineThemeOverride = {
-  colorScheme: 'light',
-  primaryColor: 'dark',
-  primaryShade: 6,
+  colorScheme: 'dark',
+  colors: {
+    white: ['#FFF'],
+  },
+  primaryColor: 'white',
+  primaryShade: 0,
   defaultRadius: 4,
+  globalStyles: ({ colors }) => ({
+    body: {
+      backgroundColor: colors.dark[6],
+      color: colors.white[0],
+    },
+  }),
   components: {
     InputWrapper: {
-      styles: {
+      styles: ({ colors }) => ({
         label: {
           fontSize: '18px',
+          color: colors.white[0],
         },
-      },
+        required: {
+          color: colors.red[6],
+        },
+      }),
+    },
+    Input: {
+      styles: ({ colors }) => ({
+        input: {
+          backgroundColor: colors.white[0],
+          height: '50px',
+          borderRadius: 0,
+          color: colors.dark[6],
+        },
+      }),
     },
     TextInput: {
-      styles: {
+      styles: ({ colors }) => ({
         input: {
           padding: '11px',
           fontSize: '18px',
           height: '50px',
+          backgroundColor: colors.white[0],
+          borderRadius: 0,
+          color: colors.dark[6],
         },
-      },
+      }),
     },
     Button: {
-      styles: {
+      styles: ({ colors }) => ({
         root: {
           height: '53px',
           fontSize: '18px',
+          color: colors.dark[6],
         },
-      },
+      }),
     },
   },
 };
