d0ff6298eec32e7c1e4106916e849176d1b5ec7b
Panos Voudouris <panos.voudouris@foolproof.co.uk>
2023-01-26 15:15:56 +0000

    Add background circle

--- apps/main/pages/index.tsx
+++ apps/main/pages/index.tsx
@@ -3,7 +3,6 @@ import { Button, Title, Stack, TextInput, Box, Flex } from '@mantine/core';
 import { useRouter } from 'next/router';
 import LayoutLogin from '../components/LayoutLogin';
 import LoginImage from '../assets/login.svg';
-import EllipseLogin from '../assets/ellipse-login.svg';
 
 export default function Home() {
   const router = useRouter();
@@ -11,46 +10,58 @@ export default function Home() {
     router.push('/check-email');
   };
   return (
-    <Stack spacing={50}>
-      <Stack spacing={24} sx={{ height: '100%' }}>
-        <Title order={1} size={20} align="center">
-          Login to your account
-        </Title>
-        <TextInput
-          placeholder="Your email address"
-          label="Your email address"
-          withAsterisk
-        />
-        <Button fullWidth onClick={handleClickContinue}>
-          Continue
-        </Button>
-      </Stack>
-      <Flex align="center" direction="column">
-        <Box
-          sx={{
-            position: 'relative',
-            width: '100%',
-            height: '100%',
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          overflow: 'hidden',
+          width: '100%',
+          height: '50%',
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: '#47D4B1',
+            width: '150vh',
+            height: '150vh',
+            position: 'absolute',
+            bottom: '-100vh',
+            left: '-75vh',
+            borderTopRightRadius: '50%',
           }}
-        >
-          <Box sx={{ zIndex: 1, position: 'relative' }}>
-            <LoginImage />
-          </Box>
+        />
+      </div>
+      <Stack spacing={50}>
+        <Stack spacing={24} sx={{ height: '100%' }}>
+          <Title order={1} size={20} align="center">
+            Login to your account
+          </Title>
+          <TextInput
+            placeholder="Your email address"
+            label="Your email address"
+            withAsterisk
+          />
+          <Button fullWidth onClick={handleClickContinue}>
+            Continue
+          </Button>
+        </Stack>
+        <Flex align="center" direction="column">
           <Box
             sx={{
-              position: 'absolute',
-              bottom: '0',
-              zIndex: 0,
-              width: '379px',
-              marginLeft: '-20px',
-              marginBottom: '-20px',
+              position: 'relative',
+              width: '100%',
+              height: '100%',
             }}
           >
-            <EllipseLogin />
+            <Box sx={{ zIndex: 1, position: 'relative' }}>
+              <LoginImage />
+            </Box>
           </Box>
-        </Box>
-      </Flex>
-    </Stack>
+        </Flex>
+      </Stack>
+    </>
   );
 }
 
