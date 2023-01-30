476d42368cd2394237e38eaeefdf3385cace1cce
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-01-30 21:59:25 +0200

    fix: type error

--- packages/collinson-design-system/src/components/searchInput/index.tsx
+++ packages/collinson-design-system/src/components/searchInput/index.tsx
@@ -7,23 +7,25 @@ export interface SearchInputProps extends TextInputProps {
   onClickClear: () => void;
 }
 
-const useStyles = createStyles(({ colors }, { value }: { value: string }) => ({
-  input: {
-    borderRadius: '8px',
-    height: value ? '50px' : '42px',
-    fontSize: '16px',
-    backgroundColor: value ? '#FFF' : colors.gray[1],
-    borderColor: value ? colors.gray[4] : colors.gray[1],
-    '&:focus': {
-      height: '50px',
-      borderColor: colors.gray[4],
-      backgroundColor: '#FFF',
-      '&::placeholder': {
-        color: 'transparent',
+const useStyles = createStyles(
+  ({ colors }, { value }: { value: TextInputProps['value'] }) => ({
+    input: {
+      borderRadius: '8px',
+      height: value ? '50px' : '42px',
+      fontSize: '16px',
+      backgroundColor: value ? '#FFF' : colors.gray[1],
+      borderColor: value ? colors.gray[4] : colors.gray[1],
+      '&:focus': {
+        height: '50px',
+        borderColor: colors.gray[4],
+        backgroundColor: '#FFF',
+        '&::placeholder': {
+          color: 'transparent',
+        },
       },
     },
-  },
-}));
+  })
+);
 
 export default function SearchInput({
   onClickClear,
