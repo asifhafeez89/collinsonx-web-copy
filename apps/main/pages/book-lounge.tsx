c20d46331374059355507bebc3db98dba946aba7
Imran Oozeerally <imran.oozeerally@collinsongroup.com>
2023-01-31 16:18:17 +0000

    fix: change mantine/core path and updates styles

--- apps/main/pages/book-lounge.tsx
+++ apps/main/pages/book-lounge.tsx
@@ -1,5 +1,13 @@
 import { Layout, Lounge, PageTitle } from '@collinson/design-system/index';
-import { Button, Title, Stack, Box, SimpleGrid, List } from '@mantine/core';
+import {
+  UnstyledButton,
+  Title,
+  Stack,
+  Box,
+  SimpleGrid,
+  List,
+  Divider,
+} from '@collinson/design-system/core';
 
 const data = {
   image:
@@ -15,16 +23,17 @@ export default function BookLounge() {
     <Stack sx={{ padding: '0 15px' }}>
       <PageTitle title={'Club Aspire Lounge'} url={'/explore-lounges'} />
       <Lounge {...data} />
+      <Divider color={'gray'} />
       <Box>
         <Title size={16} color={'#000000'}>
           Facilities
         </Title>
         <SimpleGrid cols={2}>
-          <List>
+          <List sx={{ color: '#000000' }}>
             <List.Item>Air conditioning</List.Item>
             <List.Item>Disabled access</List.Item>
           </List>
-          <List>
+          <List sx={{ color: '#000000' }}>
             <List.Item>WiFi</List.Item>
             <List.Item>Television</List.Item>
           </List>
@@ -34,7 +43,7 @@ export default function BookLounge() {
         <Title size={16} color={'#000000'}>
           Conditions
         </Title>
-        <List>
+        <List sx={{ color: '#000000' }}>
           <List.Item>
             Access is permitted no more than 3 hours prior to scheduled flight.
           </List.Item>
@@ -44,9 +53,19 @@ export default function BookLounge() {
         </List>
       </Box>
       <Box>
-        <Button color="dark" size="md" fullWidth>
+        <UnstyledButton
+          sx={{
+            borderRadius: 8,
+            background: '#000000',
+            color: '#ffffff',
+            padding: '12px 24px',
+            width: '100%',
+            textAlign: 'center',
+            fontSize: '18px',
+          }}
+        >
           Book lounge
-        </Button>
+        </UnstyledButton>
       </Box>
     </Stack>
   );
