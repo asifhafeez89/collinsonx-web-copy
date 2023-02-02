c5eaa551f99104899685c48e98f329d665b4950b
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-02-02 14:33:45 +0200

    feat: create booking badge

---
+++ apps/lounge/components/BookingBadge.tsx
@@ -0,0 +1,60 @@
+import {
+  Badge as MBadge,
+  BadgeProps as MBadgeProps,
+  Box,
+  createStyles,
+} from '@collinsonx/design-system/core';
+
+export type BookingStatus = 'pending' | 'declined' | 'confirmed';
+
+export interface BadgeProps extends MBadgeProps {
+  status: 'pending' | 'confirmed' | 'declined';
+}
+
+const useStyles = createStyles(
+  ({ colors }, { status }: { status: BadgeProps['status'] }) => {
+    return {
+      root: {
+        borderRadius: 8,
+        textTransform: 'none',
+        fontSize: 16,
+        height: 41,
+        fontWeight: 600,
+        padding: '8px 16px',
+      },
+      declined: {
+        backgroundColor: colors.red[7],
+        color: '#FFF',
+      },
+      pending: {
+        backgroundColor: colors.yellow[6],
+        color: '#000',
+      },
+      confirmed: {
+        backgroundColor: colors.cyan[6],
+        color: '#000',
+      },
+    };
+  }
+);
+
+export default function BookingBadge({
+  children,
+  status,
+  ...props
+}: BadgeProps) {
+  const { classes, cx } = useStyles({ status });
+
+  return (
+    <MBadge
+      {...props}
+      className={cx(classes.root, {
+        [classes.declined]: status === 'declined',
+        [classes.pending]: status === 'pending',
+        [classes.confirmed]: status === 'confirmed',
+      })}
+    >
+      <Box>{children}</Box>
+    </MBadge>
+  );
+}
