db2d76b0884a7d94c80044fbe11ee055149830d7
Panos Voudouris <panos.voudouris@foolproof.co.uk>
2023-02-06 21:47:33 +0000

    Add dev env files

--- packages/collinsonx-utils/src/apollo.ts
+++ packages/collinsonx-utils/src/apollo.ts
@@ -3,6 +3,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const graphqlUrl =
   process.env.GRAPHQL_API_URL ?? process.env.NEXT_PUBLIC_GRAPHQL_API_URL;
 
+console.log(graphqlUrl);
+
 export const client = new ApolloClient({
   uri: graphqlUrl,
   cache: new InMemoryCache(),
