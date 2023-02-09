6abc4de55791c97b2a6084ff1f835acab6af6725
arismarko <aris.markogiannakis@foolproof.co.uk>
2023-02-09 10:56:32 +0000

    further update

--- apps/main/config/appInfo.ts
+++ apps/main/config/appInfo.ts
@@ -3,6 +3,6 @@ const port = process.env.APP_PORT || 3000
 export const appInfo = {
   appName: 'Retell',
   apiBasePath: "/api/auth",
-  apiDomain: 'https://authz.lifestyle-x.io',
-  websiteDomain: 'https://localhost:3000'
+  apiDomain: 'http://localhost:3000',
+  websiteDomain: 'http://localhost:3000'
 }
