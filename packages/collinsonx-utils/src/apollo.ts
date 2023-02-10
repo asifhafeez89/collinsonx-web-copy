7d91d077604e823b04c564b5fdee05f428e4e2f5
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-02-10 13:57:09 +0200

    feat: add onError link

--- packages/collinsonx-utils/src/apollo.ts
+++ packages/collinsonx-utils/src/apollo.ts
@@ -1,11 +1,37 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { onError } from '@apollo/link-error';
 
 const graphqlUrl =
   process.env.GRAPHQL_API_URL ?? process.env.NEXT_PUBLIC_GRAPHQL_API_URL;
 
-export const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: graphqlUrl,
+  headers: {},
+});
+
+export const client = new ApolloClient({
   cache: new InMemoryCache(),
+  ssrMode: typeof window === 'undefined',
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError)
+        console.log(
+          `[Network error]: ${networkError}. Backend is unreachable. Is it running?`
+        );
+    }),
+    httpLink,
+  ]),
 });
 
 export { gql, ApolloProvider, useQuery, useLazyQuery } from '@apollo/client';
