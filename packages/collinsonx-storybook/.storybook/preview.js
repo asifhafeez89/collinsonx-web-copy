3e9911b4af3e1416df26345358fb95abc0186e5a
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-02-15 17:55:42 +0200

    feat: install utils package inside storybook

--- packages/collinsonx-storybook/.storybook/preview.js
+++ packages/collinsonx-storybook/.storybook/preview.js
@@ -1,1 +1,37 @@
+import { MantineProvider } from '@collinsonx/utils/core';
+import { themeLight, themeDark } from '@collinsonx/utils/themes';
+
+const withTheme = (StoryFn, context) => {
+  const theme = context.parameters.theme || context.globals.theme;
+  const storyTheme = theme === 'dark' ? themeDark : themeLight;
+
+  return (
+    <MantineProvider theme={storyTheme} withGlobalStyles withNormalizeCSS>
+      <StoryFn />
+    </MantineProvider>
+  );
+};
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      // The icon for the toolbar item
+      icon: 'circlehollow',
+      // Array of options
+      items: [
+        { value: 'light', icon: 'circlehollow', title: 'light' },
+        { value: 'dark', icon: 'circle', title: 'dark' },
+      ],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+    },
+  },
+};
+
+// export all decorators that should be globally applied in an array
+export const decorators = [withTheme];
+
 export const parameters = { layout: 'fullscreen' };
