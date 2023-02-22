1ab0453378f65c3471374dcab9a78f01a377f5ec
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-02-22 18:18:31 +0200

    feat: make switch login logo per theme set

--- packages/collinsonx-design-system/src/assets/logo/index.ts
+++ packages/collinsonx-design-system/src/assets/logo/index.ts
@@ -1,3 +1,4 @@
 export { default as LogoExperienceX } from './logo-experienceX.svg';
 export { default as LogoDinersClub } from './logo-dinersClub.svg';
+export { default as LogoDinersClubWhite } from './logo-dinersClub-white.svg';
 export { default as LogoAmex } from './logo-amex.svg';
