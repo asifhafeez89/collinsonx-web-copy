18ade16564bd6205f9dd52cbbb31ec775ac70167
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-03-21 18:07:03 +0200

    feat: update details notification; remove amount of travellers in details

--- apps/partner-management/pages/details/[id].tsx
+++ apps/partner-management/pages/details/[id].tsx
@@ -12,57 +12,84 @@ import { Pending } from '@collinsonx/design-system/assets/icons';
 import BookingButton from '@components/Details/DetailsButton';
 import { GetServerSideProps } from 'next';
 import DetailsView from '@components/Details';
+import Notification from '@components/Notification';
+import { useQuery } from '@collinsonx/utils/apollo';
+import { Booking, BookingStatus } from '@collinsonx/utils';
+import { getBookingByID } from '@collinsonx/utils/queries';
 const { bookings, lounge } = bookingsMock;
 
 interface DetailsProps {
   id: string;
 }
 
+const { Initialized, Confirmed, CheckedIn, Declined } = BookingStatus;
+
+const messageMap: Record<BookingStatus[number], string> = {
+  [Initialized]: 'Booking pending',
+  [Confirmed]: 'Booking confirmed',
+  [CheckedIn]: 'Booking confirmed',
+  [Declined]: 'Booking declined',
+};
+
 export default function Details({ id }: DetailsProps) {
+  const { loading, error, data } = useQuery<{ getBookingByID: Booking }>(
+    getBookingByID,
+    {
+      variables: { id },
+    }
+  );
+
+  const status = data?.getBookingByID?.status;
+
   const handleClickDecline = () => {};
   const handleClickConfirm = () => {};
+
   return (
-    <Stack spacing={32}>
-      <Box>
-        <Title mb={8} size={32}>
-          Customer booking details
-        </Title>
-        <Text size={18}>{lounge.name}</Text>
-      </Box>
-      <Grid>
-        <Grid.Col lg={8} md={8}>
-          <Box
-            maw={712}
-            p={40}
-            sx={{ borderRadius: 4, border: '1px solid #DDDDDD' }}
-          >
-            <DetailsView bookingId={id}>
-              <Flex w="100%" justify="flex-end" gap={32}>
-                <BookingButton variant="danger" onClick={handleClickDecline}>
-                  Decline
-                </BookingButton>
-                <BookingButton variant="success" onClick={handleClickConfirm}>
-                  Confirm
-                </BookingButton>
-              </Flex>
-            </DetailsView>
+    <>
+      {status && (
+        <Notification type={status}>{messageMap[status]}</Notification>
+      )}
+      <Box py={40} px={32}>
+        <Stack spacing={32}>
+          <Box>
+            <Title mb={8} size={32}>
+              Customer booking details
+            </Title>
+            <Text size={18}>{lounge.name}</Text>
           </Box>
-        </Grid.Col>
-      </Grid>
-    </Stack>
+          <Grid>
+            <Grid.Col lg={8} md={8}>
+              <Box
+                maw={712}
+                p={40}
+                sx={{ borderRadius: 4, border: '1px solid #DDDDDD' }}
+              >
+                <DetailsView booking={data?.getBookingByID} loading={loading}>
+                  {status === Initialized ? (
+                    <Flex w="100%" justify="flex-end" gap={32}>
+                      <BookingButton
+                        variant="danger"
+                        onClick={handleClickDecline}
+                      >
+                        Decline
+                      </BookingButton>
+                      <BookingButton
+                        variant="success"
+                        onClick={handleClickConfirm}
+                      >
+                        Confirm
+                      </BookingButton>
+                    </Flex>
+                  ) : undefined}
+                </DetailsView>
+              </Box>
+            </Grid.Col>
+          </Grid>
+        </Stack>
+      </Box>
+    </>
   );
 }
-
-const SubHeader = () => {
-  return (
-    <Box w="100%" px={40} py={16} bg="#FFF3BF">
-      <Flex gap={8} align="center">
-        <Pending />
-        <Text>Booking pending</Text>
-      </Flex>
-    </Box>
-  );
-};
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const id = ctx.params?.id as string;
@@ -78,6 +105,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   };
 };
 
-Details.getLayout = (page: JSX.Element) => (
-  <Layout subHeader={<SubHeader />}>{page}</Layout>
-);
+Details.getLayout = (page: JSX.Element) => {
+  return <Layout hasPadding={false}>{page}</Layout>;
+};
