fb083952a3cd31fbdd84f1416d98d0ad54e81b4a
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-04-04 20:54:34 +0300

    chore: cleanup

--- apps/partner-management/pages/bookings/[type].tsx
+++ apps/partner-management/pages/bookings/[type].tsx
@@ -221,8 +221,6 @@ export default function Bookings({ type }: BookingsProps) {
     [bookingId, bookings]
   );
 
-  console.log('bookings ', bookings);
-
   return (
     <>
       <BookingModal booking={selectedBooking} onClickClose={handleClickClose}>
