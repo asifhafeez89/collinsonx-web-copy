193c875abda556f8e78f1860114b552930c52ae7
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-04-06 12:07:13 +0300

    chore: workaround for build errors

--- apps/main/pages/_error.tsx
+++ apps/main/pages/_error.tsx
@@ -1,16 +1,51 @@
-import ErrorPage from '@collinsonx/pages/error';
-import LayoutLogin from '@components/LayoutLogin';
+import { Flex, Stack, Text } from '@collinsonx/design-system/core';
+
+type NextPageContext = any;
+
+async function reportError(contextData: NextPageContext) {
+  if (contextData.err) {
+    console.log('reporting error to error service:');
+    console.log(`  message: ${contextData.err.message}`);
+    console.log(`  stack: ${contextData.err.stack}`);
+    console.log('done reporting error.');
+  } else {
+    /*
+    console.log(
+      'An error occurred, but there was no error information in the NextPageContext:'
+    );
+    console.log(`  path: ${contextData.asPath}`);
+    console.log(`  page: ${contextData.pathname}`);
+    console.log(`  status: ${contextData.res?.statusCode}`);
+    */
+  }
+}
 
 interface GenericErrorProps {
   statusCode: number;
 }
 
-export default function GenericError(props: GenericErrorProps) {
+const errorMap = {
+  404: 'Page not found',
+  default: 'An unexpected error has occurred',
+};
+
+export default function GenericError({ statusCode }: GenericErrorProps) {
   return (
-    <LayoutLogin>
-      <ErrorPage {...props} />
-    </LayoutLogin>
+    <Flex justify="center" align="center" sx={{ height: '100%' }}>
+      <Text>
+        {statusCode
+          ? `${statusCode} | ${
+              errorMap[statusCode as keyof typeof errorMap] || errorMap.default
+            }`
+          : errorMap.default}
+      </Text>
+    </Flex>
   );
 }
 
-GenericError.getInitialProps = ErrorPage.getInitialProps;
+GenericError.getInitialProps = async (props: NextPageContext) => {
+  const { res, err } = props;
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  reportError(props);
+  return { statusCode };
+};
