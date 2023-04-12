bbc99d61c688416c36a0cb35322cd573210650fc
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-04-12 16:55:06 +0300

    feat: carousel images in lounges initial drop

--- apps/lounge/components/LoungeImage.tsx
+++ apps/lounge/components/LoungeImage.tsx
@@ -1,35 +1,64 @@
-import NextImage from 'next/image';
 import { Image, Maybe } from '@collinsonx/utils/generatedTypes/graphql';
+import { Carousel } from '@collinsonx/design-system/carousel';
+import { getStylesRef, createStyles } from '@collinsonx/design-system/core';
 
 export interface LoungeImageProps {
   images?: Maybe<Maybe<Image>[]>;
   width: number;
   height: number;
 }
+
+const useStyles = createStyles(() => ({
+  controls: {
+    ref: getStylesRef('controls'),
+    transition: 'opacity 150ms ease',
+    opacity: 0,
+  },
+  control: {
+    '&[data-inactive]': {
+      opacity: 0,
+      cursor: 'default',
+    },
+  },
+
+  root: {
+    '&:hover': {
+      [`& .${getStylesRef('controls')}`]: {
+        opacity: 1,
+      },
+    },
+  },
+}));
+
 const LoungeImage = ({ images, width, height }: LoungeImageProps) => {
-  if (images && images.length) {
-    if (images[0] !== null) {
-      const { url, altText } = images[0];
-      if (url) {
-        return (
-          <NextImage
-            src={url}
-            height={height}
-            width={width}
-            alt={altText || 'Image'}
-          />
-        );
-      }
-    }
+  const { classes } = useStyles();
+
+  if (images && images.length > 1) {
+    return (
+      <Carousel classNames={classes}>
+        {images.map((img, index) => (
+          <Carousel.Slide mah={127} key={img?.id ?? index}>
+            <img
+              alt={img?.altText ?? ''}
+              src={img?.url!}
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+          </Carousel.Slide>
+        ))}
+      </Carousel>
+    );
   }
-  return (
-    <NextImage
-      src="https://cdn03.collinson.cn/lounge-media/image/BHX6-13756.jpg"
-      height={height}
-      width={width}
-      alt="Default image"
-    />
-  );
+  if (images && images.length === 1) {
+    return images.map((img, index) => (
+      <img
+        key={img?.id ?? index}
+        alt={img?.altText ?? ''}
+        src={img?.url!}
+        style={{ width: '100%' }}
+      />
+    ));
+  }
+  return <></>;
 };
 
 export default LoungeImage;
