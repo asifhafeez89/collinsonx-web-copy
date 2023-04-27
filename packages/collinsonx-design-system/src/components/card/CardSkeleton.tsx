cd3717f2ebe1285447b0b6cd471a6fe3e7b5a08d
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-04-27 10:47:04 +0300

    feat: add skeleton screens for booking page

---
+++ packages/collinsonx-design-system/src/components/card/CardSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Box, Skeleton } from '@mantine/core';
+
+const CardSkeleton = () => {
+  return (
+    <>
+      <Skeleton w="100%" h={304} />
+    </>
+  );
+};
+
+export default CardSkeleton;
