9c4079e4d836529f7483df2708ca6c0fcf2ac447
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-05-19 13:10:23 +0300

    fix: remove deprecated fields and queries

--- packages/collinsonx-utils/src/queries/getConsumerByEmailAddress.ts
+++ packages/collinsonx-utils/src/queries/getConsumerByEmailAddress.ts
@@ -3,17 +3,7 @@ import { gql } from '../apollo';
 const getConsumerByEmailAddress = gql`
   query GetConsumerByEmailAddress($emailAddress: String!) {
     getConsumerByEmailAddress(emailAddress: $emailAddress) {
-      bookings {
-        bookedFrom
-        bookedTo
-        id
-        status
-        updatedAt
-      }
-      createdAt
-      emailAddress
       id
-      updatedAt
     }
   }
 `;
