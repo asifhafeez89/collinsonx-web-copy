1d42b370dbd8d1693c52e6d6f686dc8c9f2fb4e1
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-05-25 15:05:08 +0300

    chore: test default snippet

--- apps/partner-management/pages/_document.tsx
+++ apps/partner-management/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { createGetInitialProps } from '@collinsonx/design-system/next';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const getInitialProps = createGetInitialProps();
 
@@ -13,6 +14,17 @@ export default class _Document extends Document {
         <body>
           <Main />
           <NextScript />
+          <script src="https://wchat.freshchat.com/js/widget.js"></script>
+          <Script strategy="lazyOnload">
+            {`
+            window.fcWidget.init({
+              token: "22d326e8-4ec9-4a59-99e3-53d05a5f5f77",
+              host: "https://wchat.freshchat.com",
+              externalId: "TestUser",     // user's id unique to your system
+              firstName: "Test",              // user's first name
+              lastName: "User",                // user's last name
+            });`}
+          </Script>
         </body>
       </Html>
     );
