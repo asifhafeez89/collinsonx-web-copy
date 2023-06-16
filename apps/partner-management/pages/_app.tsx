7704b49bd4311f7e283a129aaa7cb813eb2245cd
arismarkoFP <aris.markogiannakis@foolproof.co.uk>
2023-06-16 14:09:30 +0100

    envlabel

--- apps/partner-management/pages/_app.tsx
+++ apps/partner-management/pages/_app.tsx
@@ -1,11 +1,10 @@
 import type { AppProps } from 'next/app';
 import { NextPage } from 'next';
-import { ComponentType, ReactElement } from 'react';
+import { ComponentType, ReactElement, useState, useEffect } from 'react';
 import { MantineProvider } from '@collinsonx/design-system/core';
 import Head from 'next/head';
 import { useApollo, ApolloProvider } from '@collinsonx/utils/apollo';
 import { Analytics } from '@vercel/analytics/react';
-
 import SuperTokens, { SuperTokensWrapper } from 'supertokens-auth-react';
 import { frontendConfig } from 'config/frontendConfig';
 
@@ -31,6 +30,19 @@ if (typeof window !== 'undefined') {
 export default function MyApp({ Component, pageProps }: Props) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
+  const [envLabel, setEnvLabel] = useState<String>('');
+
+  useEffect(() => {
+    if (
+      window.location.href.includes('https://partner-uat.test.lifestyle-x.io/')
+    ) {
+      setEnvLabel('test');
+    }
+
+    if (window.location.href.includes('http://localhost:3010/')) {
+      setEnvLabel('dev');
+    }
+  }, []);
 
   const apolloClient = useApollo(pageProps, true, PARTNER_ID);
   return (
@@ -46,6 +58,11 @@ export default function MyApp({ Component, pageProps }: Props) {
         <SuperTokensWrapper>
           <AuthWrapper>
             <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+              {envLabel !== '' && (
+                <div style={{ position: 'absolute', background: 'yellow' }}>
+                  {envLabel}
+                </div>
+              )}
               {getLayout(<Component {...pageProps} />)}
               <Analytics />
             </MantineProvider>
