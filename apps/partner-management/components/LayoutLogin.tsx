85f369e4e0a7898afaffc4e5176d78de6eb62d47
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-06-19 19:39:00 +0300

    feat: add new logo

--- apps/partner-management/components/LayoutLogin.tsx
+++ apps/partner-management/components/LayoutLogin.tsx
@@ -23,7 +23,8 @@ export default function LayoutLogin({
     >
       <header
         style={{
-          backgroundColor: '#112132',
+          backgroundColor: '#E6E6E8',
+          borderBottom: '1px solid #A8A8AA',
           width: '100%',
           padding: '1rem',
           textAlign: 'center',
