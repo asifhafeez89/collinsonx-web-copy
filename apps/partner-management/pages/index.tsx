d2f2ce4d194929a01bde2e30fdbddf44888000e9
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-06-22 10:56:40 +0300

    chore: remove default

--- apps/partner-management/pages/index.tsx
+++ apps/partner-management/pages/index.tsx
@@ -129,7 +129,7 @@ export default function Overview() {
 
   return (
     <>
-      {false && error && isErrorValid(error) ? (
+      {error && isErrorValid(error) ? (
         <Error error={error} />
       ) : (
         <>
