e00e1ce4556ebe964ea4f464b94d145a5d845978
davidwoods <david.woods@collinsongroup.com>
2023-06-22 13:56:38 +0100

    chore: refactor _getOTP() to use await instead of promises

--- packages/collinsonx-tests-e2e/tests/actions/Login.js
+++ packages/collinsonx-tests-e2e/tests/actions/Login.js
@@ -30,40 +30,27 @@ class Login {
     await this._helper.wait(5000);
   }
 
-  async _getOTP(uuid) {
+  async _getOTP(user) {
     let id;
+    
     const mailinatorClient = new MailinatorClient(
       '2a32de31d6734501abb238da21c9ac3a'
     );
 
-    await mailinatorClient
-      .request(new GetInboxRequest('clearrouteteam.testinator.com'))
-      .then((response) => {
-        const result = response.result;
-        const msgs = result?.msgs;
+    const inbox = await mailinatorClient.request(
+      new GetInboxRequest('clearrouteteam.testinator.com')
+      );
 
-        if (msgs !== undefined) {
-          msgs.forEach((msg) => {
-            const to = msg.to;
-            
-            if (to == uuid) {
-              id = msg.id;
-            }
-          });
-        }
-      });
+    const latestMessage = inbox.result.msgs.find(message => message.to === user);
+    id = latestMessage.id;
+   
+    const otp = await mailinatorClient.request(
+      new GetMessageRequest('clearrouteteam.testinator.com', user, id)
+    );
+    const regex = /(\d{6})<\/div>/g;
+    const match = regex.exec(otp.result.parts[0].body);
 
-    const otp = await mailinatorClient
-      .request(new GetMessageRequest('clearrouteteam.testinator.com', uuid, id))
-      .then((response) => {
-        const result = response.result;
-        const parts = result?.parts;
-        const regex = /(\d{6})<\/div>/g;
-        let match = regex.exec(parts[0].body);
-
-        return match[1].toString();
-      });
-    return otp;
+    return match[1].toString();
   }
 }
 
