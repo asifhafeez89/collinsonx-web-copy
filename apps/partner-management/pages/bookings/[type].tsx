502bba7c9706571506f0dbce7a39189624683716
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-06-22 14:54:11 +0300

    fix: modal content flex

--- apps/partner-management/pages/bookings/[type].tsx
+++ apps/partner-management/pages/bookings/[type].tsx
@@ -403,7 +403,20 @@ export default function Bookings({ type }: BookingsProps) {
 
   return (
     <>
-      <Modal opened={bookingId !== null} onClose={handleCloseModal}>
+      <Modal
+        opened={bookingId !== null}
+        onClose={handleCloseModal}
+        sx={{
+          '.mantine-Modal-content': {
+            flex: 'none',
+          },
+        }}
+        styles={{
+          content: {
+            flex: 'none',
+          },
+        }}
+      >
         <Details booking={selectedBooking}>
           <DetailsConfirmedActions
             checkIn={checkIn}
