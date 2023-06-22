287a1407e22d9f2a35cdbbd6f20bca7a1e2544a1
davidwoods <david.woods@collinsongroup.com>
2023-06-22 16:30:48 +0100

    chore: add to login test - check the marketing box and click login

--- packages/collinsonx-tests-e2e/tests/actions/SignUp.js
+++ packages/collinsonx-tests-e2e/tests/actions/SignUp.js
@@ -4,6 +4,18 @@ class SignUp {
   constructor(page) {
     this._signUpPage = new SignUpPage(page);
   };
+
+  async fillInDetails() {
+    const user = ['Test', 'User']
+    await this._signUpPage.getFirstNameTextbox().fill(user[0]);
+    await this._signUpPage.getLastNameTextbox().fill(user[1]);
+
+    // add datepicker if possible
+
+    await this._signUpPage.getMarketingCheckbox().click();
+
+    await this._signUpPage.getLoginButton().click();
+  };
 };
 
 module.exports = SignUp;
--- packages/collinsonx-tests-e2e/tests/specs/goToLoginPage.spec.js
+++ packages/collinsonx-tests-e2e/tests/specs/goToLoginPage.spec.js
@@ -1,14 +1,17 @@
 const { test, expect } = require('@playwright/test');
 import Login from '../actions/Login';
+import SignUp from '../actions/SignUp';
 import HasTheUserLoggedIn from '../questions/HasTheUserLoggedIn';
 
 test('login as a new partner', async ({ page }) => {
   //Given
   let login = new Login(page);
+  let signUp = new SignUp(page);
 
   //When
   await login.goTo();
   await login.login();
+  await signUp.fillInDetails();
 
   //Then
   let hasTheUserLoggedIn = new HasTheUserLoggedIn(expect);
