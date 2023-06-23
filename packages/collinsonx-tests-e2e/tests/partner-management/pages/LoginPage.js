30c2febe305b355eec946d0eae1cba9ae5167e95
davidwoods <david.woods@collinsongroup.com>
2023-06-23 15:19:16 +0100

    chore: add elements to the loginpage to be used in the actions directory

--- packages/collinsonx-tests-e2e/tests/partner-management/pages/LoginPage.js
+++ packages/collinsonx-tests-e2e/tests/partner-management/pages/LoginPage.js
@@ -3,6 +3,22 @@ class LoginPage {
     this.page = page;
   };
 
+  enterEmailAddress(email) {
+    this.page.getByTestId('email').fill(email);
+  };
+
+  enterPassword(password) {
+    this.page.getByTestId('password').fill(password);
+  };
+
+  saveMyPassword() {
+    this.page.getByTestId('saveMyPassword').click();
+  };
+
+  login() {
+    this.page.getByTestId('login').click();
+  };
+
 };
   
 module.exports = LoginPage;
