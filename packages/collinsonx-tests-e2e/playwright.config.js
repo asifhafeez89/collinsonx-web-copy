10131a253ac0028ca6eea4a27da97f9284f17a71
davidwoods <david.woods@collinsongroup.com>
2023-06-23 10:13:06 +0100

    chore: increase timeout from default 30s to 50s for tests to run

--- packages/collinsonx-tests-e2e/playwright.config.js
+++ packages/collinsonx-tests-e2e/playwright.config.js
@@ -11,6 +11,8 @@ require('dotenv').config();
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
+  // max time (ms) for tests inc. teardown
+  timeout: 60000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
