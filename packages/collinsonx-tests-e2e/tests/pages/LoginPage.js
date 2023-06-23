2b6bbef07cdabfa8fd7ed8a7b72e59ae054034d2
davidwoods <david.woods@collinsongroup.com>
2023-06-23 10:20:01 +0100

    chore: update formatting, refactor getHomePageTitle() to get element by testid

--- packages/collinsonx-tests-e2e/tests/pages/LoginPage.js
+++ packages/collinsonx-tests-e2e/tests/pages/LoginPage.js
@@ -1,28 +1,23 @@
 class LoginPage {
   constructor(page) {
     this.page = page;
-  }
+  };
 
   getURL() {
     return this.page.goto('http://localhost:3000');
-  }
+  };
 
   getEmailAddressTextbox() {
     return this.page.getByTestId('loginEmailAddress');
-  }
-
-  getCollinsonTitle() {
-    return this.page.toHaveTitle('CollinsonX');
-  }
+  };
 
   getHomePageTitle() {
-    return this.page.getByRole('button', {
-      name: 'Ready for your next experience?',
-    });
-  }
+    return this.page.getByTestId('homePageTitle');
+  };
 
   getLoginButton() {
     return this.page.getByTestId('login');
-  }
-}
+  };
+};
+
 module.exports = LoginPage;
