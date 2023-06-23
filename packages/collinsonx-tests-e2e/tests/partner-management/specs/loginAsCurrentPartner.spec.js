722605b50bdce7ead171c41e073f5aadd7d72387
davidwoods <david.woods@collinsongroup.com>
2023-06-23 16:02:01 +0100

    chore: add login step to login test - will need to add assertions

--- packages/collinsonx-tests-e2e/tests/partner-management/specs/loginAsCurrentPartner.spec.js
+++ packages/collinsonx-tests-e2e/tests/partner-management/specs/loginAsCurrentPartner.spec.js
@@ -1,5 +1,8 @@
 const { test } = require('@playwright/test');
+import Login from '../actions/Login';
 
 test('login as a new user', async ({ page }) => {
-
+    let login = new Login();
+    
+    await login.asNewUser(false);
 });
