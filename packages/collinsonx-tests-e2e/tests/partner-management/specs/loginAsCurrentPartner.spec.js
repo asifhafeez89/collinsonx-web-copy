244d3281a47081227a3100e2dddd7b0447caacad
davidwoods <david.woods@collinsongroup.com>
2023-06-26 09:47:06 +0100

    chore: rename current partner login test

--- packages/collinsonx-tests-e2e/tests/partner-management/specs/loginAsCurrentPartner.spec.js
+++ packages/collinsonx-tests-e2e/tests/partner-management/specs/loginAsCurrentPartner.spec.js
@@ -1,7 +1,7 @@
 const { test } = require('@playwright/test');
 import Login from '../actions/Login';
 
-test('login as a new user', async ({ page }) => {
+test('login as a current partner', async ({ page }) => {
     let login = new Login(page);
 
     await login.asNewUser(false);
