84b24a6050c0d94f5f413b4eff12e2ace34fd997
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-06-27 16:08:29 +0300

    feat: cookie banner initial drop

---
+++ apps/partner-management/components/CookieBanner.tsx
@@ -0,0 +1,37 @@
+import { Button, Flex, Text, Portal } from '@collinsonx/design-system/core';
+import styled from '@collinsonx/design-system/styled';
+
+const Container = styled.div`
+  height: 92px;
+  background-color: #f4f4f4;
+  color: #000;
+  box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.25);
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+
+  /* freshchat widget zIndex = 2147483600 */
+  z-index: 2147483601;
+`;
+const CookieBanner = () => {
+  const handleClickAccept = () => {};
+  const handleClickDecline = () => {};
+  return (
+    <Portal>
+      <Container>
+        <Flex h="100%" gap={48} align="center" justify="center">
+          <Text size={18}>
+            This website uses cookies to ensure you get the best experience.
+            Learn more
+          </Text>
+          <Flex gap={16}>
+            <Button onClick={handleClickAccept}>Accept</Button>
+            <Button onClick={handleClickDecline}>Decline</Button>
+          </Flex>
+        </Flex>
+      </Container>
+    </Portal>
+  );
+};
+
+export default CookieBanner;
