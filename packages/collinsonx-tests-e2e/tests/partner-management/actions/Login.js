8f5ea2b95d60437e95bff7bd2dd620dba474feca
davidwoods <david.woods@collinsongroup.com>
2023-06-27 17:14:50 +0100

    refactor login action for partner app

--- packages/collinsonx-tests-e2e/tests/partner-management/actions/Login.js
+++ packages/collinsonx-tests-e2e/tests/partner-management/actions/Login.js
@@ -5,34 +5,17 @@ class Login {
     this._loginPage = new LoginPage(page);
   };
 
-  async asNewUser(boolean) {
+  async login(email, password) {    
     await this._loginPage.goToURL();
-
-    const email = this.setPartnerEmail(boolean);
 
     await this._loginPage.enterEmailAddress(email);
 
-    // password will be changed and added to secret variables at a later date
-    await this._loginPage.enterPassword('CollinsonXPartner123');
+    await this._loginPage.enterPassword(password);
 
     await this._loginPage.saveMyPassword();
 
     await this._loginPage.login();
   };
-
-  // logic for new user is not currently used - will be added after current user tests
-  setPartnerEmail(isNewPartner) {
-    let partner;
-
-    if (isNewPartner == true) {
-      partner = uuidv4();
-    } else {
-      partner = 'automationuserpartner';
-    }
-
-    return `${partner}@clearrouteteam.testinator.com`;
-  };
-
 };
 
 module.exports = Login;
