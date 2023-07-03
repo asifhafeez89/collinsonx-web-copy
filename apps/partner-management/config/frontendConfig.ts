d92eed0b2c07095862fa33e312f15f8e0cf047f8
arismarkoFP <aris.markogiannakis@foolproof.co.uk>
2023-07-03 15:06:31 +0100

    added chnage for session init

--- apps/partner-management/config/frontendConfig.ts
+++ apps/partner-management/config/frontendConfig.ts
@@ -1,12 +1,18 @@
-import Session from 'supertokens-auth-react/recipe/session';
+import Session, { InputType } from 'supertokens-auth-react/recipe/session';
 import EmailPassword from 'supertokens-auth-react/recipe/emailpassword';
 import Router from 'next/router';
 import { appInfo } from './appInfo';
 
 export const frontendConfig = () => {
+  const sessionTokenFrontendDomain = process.env.NEXT_PUBLIC_SESSION_SCOPE;
+
   return {
     appInfo,
-    recipeList: [Session.init(), EmailPassword.init()],
+    recipeList: [
+      // Session.init({ sessionTokenFrontendDomain } as InputType),
+      Session.init({ sessionTokenFrontendDomain } as InputType),
+      EmailPassword.init(),
+    ],
     windowHandler: (oI: any) => {
       return {
         ...oI,
