7cb346ddc1230e30fe66c49059fed8ceb2ddf4dc
davidwoods <david.woods@clearroute.co.uk>
2023-07-12 12:09:50 +0100

    chore: remove whitespace

--- packages/collinsonx-tests-e2e/tests/lounge/pages/CheckEmailPage.js
+++ packages/collinsonx-tests-e2e/tests/lounge/pages/CheckEmailPage.js
@@ -1,13 +1,11 @@
 class CheckEmailPage {
   constructor(page) {
-      this.page = page;
-    };
-    
+    this.page = page;
+  };
+
   getVerifyButton() {
     return this.page.getByTestId('verify');
   };
-
-  
 };
 
 module.exports = CheckEmailPage;
