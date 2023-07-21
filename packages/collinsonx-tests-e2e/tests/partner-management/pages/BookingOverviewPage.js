d4be4567b30d4e384307456ef08e18eca032c670
davidwoods <david.woods@clearroute.co.uk>
2023-07-21 16:22:23 +0100

    refactor: getPendingRequestCount function to reduce flaky tests

--- packages/collinsonx-tests-e2e/tests/partner-management/pages/BookingOverviewPage.js
+++ packages/collinsonx-tests-e2e/tests/partner-management/pages/BookingOverviewPage.js
@@ -1,55 +1,65 @@
 class BookingOverviewPage {
-    constructor(page) {
-        this.page = page;
-    };
+  constructor(page) {
+    this.page = page;
+  };
 
-    getPageTitle() {
-        return this.page.getByTestId('bookingOverviewTitle');
-    };
+  getPageTitle() {
+    return this.page.getByTestId('bookingOverviewTitle');
+  };
 
-    getLoungeTitle() {
-        return this.page.getByTestId('loungeTitle');
-    };
+  getLoungeTitle() {
+    return this.page.getByTestId('loungeTitle');
+  };
 
-    getPendingRequestsTitle() {
-        return this.page.getByTestId('pendingRequestsTitle');
-    };
+  getPendingRequestsTitle() {
+    return this.page.getByTestId('pendingRequestsTitle');
+  };
 
-    getConfirmedBookingsTitle() {
-        return this.page.getByTestId('confirmedBookingsTitle');
-    };
+  getConfirmedBookingsTitle() {
+    return this.page.getByTestId('confirmedBookingsTitle');
+  };
 
-    getWalkupQRcodeTitle() {
-        return this.page.getByTestId('walkupQRcodeTitle');
-    };
+  getWalkupQRcodeTitle() {
+    return this.page.getByTestId('walkupQRcodeTitle');
+  };
 
-    getCancelledBookingsTitle() {
-        return this.page.getByTestId('cancelledBookingsTitle');
-    };
+  getCancelledBookingsTitle() {
+    return this.page.getByTestId('cancelledBookingsTitle');
+  };
 
-    viewAllPendingRequests() {
-        return this.page.getByTestId('viewAllPendingRequests').click();
-    };
+  viewAllPendingRequests() {
+    return this.page.getByTestId('viewAllPendingRequests').click();
+  };
 
-    viewAllDeclined() {
-        return this.page.getByTestId('viewAllDeclined').click();
-    };
+  viewAllDeclined() {
+    return this.page.getByTestId('viewAllDeclined').click();
+  };
 
-    viewAllConfirmed() {
-        return this.page.getByTestId('viewAllConfirmed').click();
-    };
+  viewAllConfirmed() {
+    return this.page.getByTestId('viewAllConfirmed').click();
+  };
 
-    async getPendingRequestCount() {
-        const element = await this.page.getByTestId('pendingRequestsCount');
-        await element.waitFor();
-        return Number(await element.innerText());
-    };
+  async getPendingRequestCount() {
+    // retry mechanism - returns displayed pending request count, sometimes returned zero despite this not being the case
+    let count = 0;
+    let attempt = 0;
 
-    async getConfirmedBookingCount() {
-        const element = await this.page.getByTestId('allBookingsCount');
-        await element.waitFor();
-        return Number(await element.innerText());
-    };
+    do {
+      const element = await this.page.getByTestId('pendingRequestsCount');
+      await element.waitFor();
+      count = Number(await element.innerText());
+      attempt++;
+      attempt !== 1 && await page.waitForTimeout(2000);
+    } while (count === 0 && attempt <= 5);
+
+    return count;
+  };
+
+  async getConfirmedBookingCount() {
+    const element = await this.page.getByTestId('allBookingsCount');
+    await element.waitFor();
+    return Number(await element.innerText());
+  };
 
 };
 
