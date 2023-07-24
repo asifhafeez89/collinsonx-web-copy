5b41434ab87abb1456f47cf4204a8778f48bab79
davidwoods <david.woods@clearroute.co.uk>
2023-07-24 10:13:50 +0100

    refactor: getbookings functions - can now accept multiple arguments and return bookings with the statuses passed in

--- packages/collinsonx-tests-e2e/tests/partner-management/utils/BookingApi.js
+++ packages/collinsonx-tests-e2e/tests/partner-management/utils/BookingApi.js
@@ -213,8 +213,8 @@ class BookingApi {
 
   };
 
-  async getBookingCount(user, status) {
-    const statusBookings = await this.getBookings(user, status);
+  async getBookingCount(user, ...statuses) {
+    const statusBookings = await this.getBookings(user, ...statuses);
 
     const statusBookingsCount = statusBookings.length;
 
@@ -275,7 +275,7 @@ class BookingApi {
     await axios.post(this.apiUrl, request, { headers });
   }
 
-  async getBookings(user, status) {
+  async getBookings(user, ...statuses) {
     const query = `
       query GetBookings($experienceId: ID!) {
         getBookings(experienceID: $experienceId) {
@@ -304,12 +304,7 @@ class BookingApi {
     const bookings = response.data.data.getBookings;
 
     const statusBookings = bookings.filter((booking) => {
-      // Both "CONFIRMED" and "CHECKED_IN" statuses appear under the confirmed bookings page
-      if (status === "CONFIRMED") {
-        return booking.status === "CONFIRMED" || booking.status === "CHECKED_IN";
-      } else {
-        return booking.status === status;
-      };
+      return statuses.includes(booking.status);
     });
 
     return statusBookings;
