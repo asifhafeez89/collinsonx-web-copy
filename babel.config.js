0d4d7d38305071f436043949a098834226721ec2
Dan Iosif <dan.iosif@collinsongroup.com>
2023-07-25 16:54:37 +0100

    chore: small refactor

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
+const shouldInstrumentCode = 'INSTRUMENT_CODE' in process.env
+console.log('shouldInstrumentCode', shouldInstrumentCode)
+
 module.exports = {
-  presets: ['next/babel'],
-  env: {
-    test: {
-      plugins: ['istanbul']
-    }
-  }
+  "presets": ["next/babel"],
+  "plugins": shouldInstrumentCode ? ["istanbul"] : []
 }
+
+console.dir(module.exports, {depth: null})
