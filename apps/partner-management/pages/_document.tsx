af680e197ed4e86c921d0c1d15b0f2d41ef9bfb7
Dan Iosif <dan.iosif@collinsongroup.com>
2023-07-27 13:02:05 +0100

    fix: crossOrigin set to empty string/anonymous

--- apps/partner-management/pages/_document.tsx
+++ apps/partner-management/pages/_document.tsx
@@ -13,7 +13,7 @@ export default class _Document extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
