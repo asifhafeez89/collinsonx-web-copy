79ed2b92d704633ebbe40c3374d9a3bc7b7d8777
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-08-16 16:53:45 +0300

    chore: upgrade storybook

--- packages/collinsonx-storybook/.storybook/main.js
+++ packages/collinsonx-storybook/.storybook/main.js
@@ -1,15 +1,20 @@
+import { dirname, join } from "path";
 module.exports = {
   stories: ['../../collinsonx-design-system/src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {}
   },
+
   staticDirs: ['../public'],
+
   webpackFinal: async (config, { configType }) => {
     config.module.rules = [
       ...config.module.rules.map((rule) => {
@@ -29,4 +34,12 @@ module.exports = {
     // Return the altered config
     return config;
   },
+
+  docs: {
+    autodocs: true
+  }
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
