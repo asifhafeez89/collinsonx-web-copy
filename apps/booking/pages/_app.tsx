8e20d2e232e390583ef035d1d099ecf55ba841ff
arismarkoFP <aris.markogiannakis@foolproof.co.uk>
2023-08-17 14:28:16 +0100

    added changes and build and merged

--- apps/booking/pages/_app.tsx
+++ apps/booking/pages/_app.tsx
@@ -1,21 +1,24 @@
 import type { AppProps } from 'next/app';
 import { NextPage } from 'next';
-import { useMediaPredicate } from 'react-media-hook';
-import { ComponentType, ReactElement, useState, useEffect } from 'react';
-import {
-  ActionIcon,
-  ColorScheme,
-  ColorSchemeProvider,
-  MantineProvider,
-  MediaQuery,
-  Grid,
-} from '@collinsonx/design-system/core';
+import { ComponentType, ReactElement } from 'react';
+import { MantineProvider } from '@collinsonx/design-system/core';
 import Head from 'next/head';
+import { frontendConfig } from '../config/frontendConfig';
+import SuperTokensReact, {
+  SuperTokensConfig,
+  SuperTokensWrapper,
+} from '@collinsonx/utils/supertokens';
+import { UserProvider } from '@collinsonx/utils/lib/userContext';
+import { useApollo, ApolloProvider } from '@collinsonx/utils/apolloBooking';
+import SessionManager from '@components/SessionManager';
 import { Analytics } from '@vercel/analytics/react';
-import theme from '../theme';
-import Link from 'next/link';
-import { IconMoonStars, IconSun } from '@tabler/icons-react';
-import { useLocalStorage } from '@mantine/hooks';
+import AuthWrapper from '@components/AuthWrapper';
+
+if (typeof window !== 'undefined') {
+  // we only want to call this init function on the frontend, so
+  // we check typeof window !== 'undefined'
+  SuperTokensReact.init(frontendConfig() as SuperTokensConfig);
+}
 
 type Page<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactElement) => JSX.Element;
@@ -27,111 +30,33 @@ type Props = AppProps & {
 };
 
 export default function MyApp({ Component, pageProps }: Props) {
-  const [envLabel, setEnvLabel] = useState<String>('');
-  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: 'mantine-color-scheme',
-    defaultValue: useMediaPredicate('(prefers-color-scheme: dark)')
-      ? 'dark'
-      : 'light',
-    getInitialValueInEffect: true,
-  });
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
-  const dark = colorScheme === 'dark';
-
-  useEffect(() => {
-    if (window.location.href.includes('https://booking.uat.lifestyle-x.io/')) {
-      setEnvLabel('uat');
-    }
-
-    if (window.location.href.includes('https://booking.test.lifestyle-x.io/')) {
-      setEnvLabel('test');
-    }
-
-    if (window.location.href.includes('http://localhost:3011/')) {
-      setEnvLabel('dev');
-    }
-  }, []);
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const apolloClient = useApollo(pageProps);
 
   return (
     <>
       <Head>
-        <title>Booking</title>
+        <title>CollinsonX</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
-      <ColorSchemeProvider
-        colorScheme={colorScheme}
-        toggleColorScheme={toggleColorScheme}
-      >
-        <MantineProvider
-          theme={{ colorScheme, ...theme }}
-          withGlobalStyles
-          withNormalizeCSS
-        >
-          {envLabel !== '' && (
-            <div
-              style={{
-                position: 'absolute',
-                background: 'yellow',
-                color: 'black',
-              }}
-            >
-              {envLabel}
-            </div>
-          )}
-          <div
-            style={{
-              width: '100%',
-              height: '100%',
-            }}
-          >
-            <MediaQuery
-              query="print"
-              styles={{
-                display: 'none',
-              }}
-            >
-              <header
-                style={{
-                  borderBottom: '1px solid #A8A8AA',
-                  // width: '100%',
-                  padding: '1rem',
-                  textAlign: 'center',
-                  // margin: 0,
-                }}
-              >
-                <Grid grow>
-                  <Grid.Col span={11}>
-                    <Link href="/">Insert Logo here</Link>
-                  </Grid.Col>
-                  <Grid.Col span={1}>
-                    <ActionIcon
-                      variant="outline"
-                      color={dark ? 'yellow' : 'blue'}
-                      onClick={() => toggleColorScheme()}
-                      title="Toggle color scheme"
-                    >
-                      {dark ? (
-                        <IconSun size="1.1rem" />
-                      ) : (
-                        <IconMoonStars size="1.1rem" />
-                      )}
-                    </ActionIcon>
-                  </Grid.Col>
-                </Grid>
-              </header>
-            </MediaQuery>
-            <main style={{ padding: '32px 40px', margin: 0 }}>
-              <Component {...pageProps} />
-            </main>
-          </div>
-          <Analytics />
-        </MantineProvider>
-      </ColorSchemeProvider>
+      <UserProvider>
+        <SuperTokensWrapper>
+          <ApolloProvider client={apolloClient}>
+            <AuthWrapper>
+              <SessionManager>
+                <MantineProvider withGlobalStyles withNormalizeCSS>
+                  {getLayout(<Component {...pageProps} />)}
+                  <Analytics />
+                </MantineProvider>
+              </SessionManager>
+            </AuthWrapper>
+          </ApolloProvider>
+        </SuperTokensWrapper>
+      </UserProvider>
     </>
   );
 }
---
+++ apps/booking/pages/index.tsx
@@ -0,0 +1,133 @@
+import { Title, Stack, Flex } from '@collinsonx/design-system/core';
+import { Button } from '@collinsonx/design-system/core';
+import { useForm } from '@collinsonx/design-system/form';
+import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from 'react';
+import LayoutLogin from '@components/LayoutLogin';
+import {
+  createPasswordlessCode,
+  useSessionContext,
+} from '@collinsonx/utils/supertokens';
+import { InputLabel } from '@collinsonx/design-system';
+import validateEmail from '@collinsonx/utils/lib/validateEmail';
+import LoaderLifestyleX from '@collinsonx/design-system/components/loaderLifestyleX';
+
+interface FormValues {
+  email: string;
+}
+
+export default function Home(props: unknown) {
+  const session = useSessionContext();
+
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+  const [loginError, setLoginError] = useState('');
+
+  const ref = useRef(false);
+
+  const form = useForm({
+    initialValues: {
+      email: '',
+    },
+    validate: {
+      email: (value: string) =>
+        validateEmail(value) ? null : 'Please enter a valid email address.',
+    },
+  });
+
+  useEffect(() => {
+    if (session && !session.loading) {
+      const { userId } = session;
+      if (userId) {
+        if (!ref.current) {
+          router.push('/booking');
+          ref.current = true;
+        }
+      } else {
+        setLoading(false);
+      }
+    }
+  }, [session, router]);
+
+  const handleClickContinue = async ({ email }: FormValues) => {
+    if (!validateEmail(email.trim())) {
+      setLoginError('Invalid email');
+    } else {
+      try {
+        await createPasswordlessCode({
+          email,
+        });
+        router.push({
+          pathname: '/checkemail',
+          query: { email, redirectUrl: router.query?.redirectUrl },
+        });
+      } catch (err: any) {
+        console.log(err);
+        if (err.isSuperTokensGeneralError === true) {
+          // this may be a custom error message sent from the API by you,
+          // or if the input email / phone number is not valid.
+          window.alert(err.message);
+        } else {
+          window.alert('Oops! Something went wrong.');
+        }
+      }
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <Flex justify="center" align="center" h="100%">
+          <LoaderLifestyleX />
+        </Flex>
+      ) : (
+        <LayoutLogin>
+          <form onSubmit={form.onSubmit(handleClickContinue)}>
+            <Stack spacing={50}>
+              <Stack spacing={24} sx={{ height: '100%' }}>
+                <Title order={1} size={20} align="center">
+                  Login to your account
+                </Title>
+                <InputLabel
+                  type="text"
+                  autoFocus
+                  placeholder="Your email address"
+                  label="Your email address"
+                  isWhite={true}
+                  styles={{
+                    root: {
+                      display: 'flex',
+                      flexDirection: 'column',
+                    },
+                    description: {
+                      order: 1,
+                      marginTop: '4px',
+                      marginBottom: '0',
+                    },
+                    label: {
+                      order: -2,
+                    },
+                    input: {
+                      order: -1,
+                    },
+                    error: {
+                      order: 2,
+                    },
+                  }}
+                  withAsterisk
+                  {...form.getInputProps('email')}
+                  data-testid="loginEmailAddress"
+                />
+
+                <Button type="submit" data-testid="login">
+                  Login
+                </Button>
+              </Stack>
+            </Stack>
+          </form>
+        </LayoutLogin>
+      )}
+    </>
+  );
+}
