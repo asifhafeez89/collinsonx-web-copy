d2b1881d41d55d0b4fd98d8e0112e00f9c06cdc7
woodsjd-cr <135109381+woodsjd-cr@users.noreply.github.com>
2023-08-25 13:33:35 +0100

    Test/nee 90 error handling (#485)
    * chore: handle error when unable to create a booking
    
    * chore: add error handling for the getRegistrationURL function within SignUp.js
    
    * chore: add error handling for deleteBooking function within bookingApi
    
    * chore: add error handling to api request in bookingApi.js
    
    * refactor: remove try catch block as to be consistent with other error handling methods
    
    * chore: handle mailinator api latency and no invitation being sent out to mailinator

--- packages/collinsonx-tests-e2e/tests/partner-management/utils/BookingApi.js
+++ packages/collinsonx-tests-e2e/tests/partner-management/utils/BookingApi.js
@@ -134,6 +134,12 @@ class BookingApi {
 
     const response = await axios.post(this.apiUrl, request, { headers });
 
+    const createBookingObj = response.data.data.createBooking;
+
+    if (createBookingObj === null) {
+      throw new Error("The response createBooking object is null. The method of authorisation, or the credentials themselves, may be incorrect.");
+    };
+
     const bookingId = response.data.data.createBooking.id;
 
     const reference = response.data.data.createBooking.reference;
@@ -259,6 +265,10 @@ class BookingApi {
     };
 
     const response = await axios.post(this.apiUrl, request, { headers });
+
+    if (response.data.errors) {
+      throw new Error(response.data.errors[0].message)
+    };
   };
 
   async confirmBooking(bookingId) {
@@ -287,6 +297,10 @@ class BookingApi {
     };
 
     const response = await axios.post(this.apiUrl, request, { headers });
+
+    if (response.data.errors) {
+      throw new Error(response.data.errors[0].message)
+    };
   };
 
   async deleteBooking(bookingId) {
@@ -312,7 +326,11 @@ class BookingApi {
       'x-user-type': 'SUPER_USER'
     };
 
-    await axios.post(this.apiUrl, request, { headers });
+    const response = await axios.post(this.apiUrl, request, { headers });
+
+    if (response.data.errors) {
+      throw new Error(response.data.errors[0].message)
+    };
   }
 
   async getBookings(lounge, ...statuses) {
@@ -341,6 +359,9 @@ class BookingApi {
     };
 
     const response = await axios.post(this.apiUrl, request, { headers });
+
+    // No error handling - unauthorised requests will always return 200 with an empty array
+
     const bookings = response.data.data.getBookings;
 
     const statusBookings = bookings.filter((booking) => {
@@ -375,6 +396,11 @@ class BookingApi {
     };
 
     const response = await axios.post(this.apiUrl, request, { headers });
+
+    if (response.data.errors) {
+      throw new Error(response.data.errors[0].message)
+    };
+
     const booking = response.data.data.getBookingByID;
 
     return booking;
