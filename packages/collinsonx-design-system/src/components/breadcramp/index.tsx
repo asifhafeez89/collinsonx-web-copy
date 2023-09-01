7f5fce9c76078cc4f0f22dd425a9ea82f1b98bf8
Frankie Bell <frankie.bell@clearroute.co.uk>
2023-09-01 11:56:14 +0100

    chore: revert breadcramp

--- packages/collinsonx-design-system/src/components/breadcramp/index.tsx
+++ packages/collinsonx-design-system/src/components/breadcramp/index.tsx
@@ -12,7 +12,7 @@ export default function Breadcramp({ title, url }: BreadcrampProps) {
     <Anchor
       href={url}
       target="_blank"
-      sx={{ width: 'fit-content' }}
+      sx={{ width: 'fit-content', textDecoration: 'none' }}
     >
       <NavLink label={title} icon={<ArrowLeft size="1rem" stroke={1.5} />} />
     </Anchor>
