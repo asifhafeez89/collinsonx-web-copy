6ddab4db2a76178a141d404714675ea38ca40251
Frankie Bell <frankie.bell@clearroute.co.uk>
2023-09-04 17:43:02 +0100

    style: update login page

--- apps/booking/pages/auth/signup-user.tsx
+++ apps/booking/pages/auth/signup-user.tsx
@@ -3,19 +3,15 @@ import {
   Stack,
   Text,
   Flex,
-  Group,
   Notification,
   Checkbox,
   Title,
 } from '@collinsonx/design-system/core';
 import { useForm } from '@collinsonx/design-system/form';
 import LayoutLogin from '../../components/LayoutLogin';
-import { Calendar } from '@collinsonx/design-system/assets/icons';
 import {
   Breadcramp,
-  DatePicker,
   InputLabel,
-  PageTitle,
 } from '@collinsonx/design-system';
 import { useState } from 'react';
 import updateConsumer from '@collinsonx/utils/mutations/updateConsumer';
@@ -26,12 +22,12 @@ import validateEmail from '@collinsonx/utils/lib/validateEmail';
 import LoaderLifestyleX from '@collinsonx/design-system/components/loaderLifestyleX';
 import Error from '@components/Error';
 import usePayload from 'hooks/payload';
+import colors from 'ui/colour-constants';
 
 export default function SignupUser() {
   const { token } = usePayload();
   const router = useRouter();
   const [loading, setLoading] = useState(false);
-  const DATE_FORMAT = 'DD/MM/YYYY';
 
   const { email } = router.query;
 
@@ -43,7 +39,6 @@ export default function SignupUser() {
       marketingConsent: false,
       dateOfBirth: new Date('1990-01-01'),
     },
-
     validate: {
       email: (value: string) =>
         validateEmail(value) ? null : 'Please enter a valid email address.',
@@ -70,9 +65,7 @@ export default function SignupUser() {
           ))}
         </Notification>
       )}
-      <Stack sx={{ width: '100%' }}>
-        <Breadcramp title="Back to Gatwick" url="https://bbc.co.uk" />
-      </Stack>
+      <Breadcramp title="Back to Gatwick" url="https://bbc.co.uk" />
       <form
         onSubmit={form.onSubmit((values: any) => {
           const consumerInput: ConsumerInput = {
@@ -104,71 +97,61 @@ export default function SignupUser() {
               width: '440px',
               margin: '0 auto',
               '@media (max-width: 40em)': {
+                padding: '16px 24px 0 24px',
                 width: '100%',
               },
             }}
           >
-            <Title order={1} size={20} align="center">
-              Register
+            <Text size={18} align='center'>
+              Your session has expired, please confirm your details
+            </Text>
+            <Title order={1} size={24} align="center">
+              Update details
             </Title>
             <Error error={error} />
+            <Stack spacing={0}>
+              <Text><Text span color={colors.red}>*</Text> First name(s)</Text>
+              <InputLabel
+                autoFocus
+                type="text"
+                withAsterisk
+                {...form.getInputProps('firstname')}
+                placeholder="First name"
+                data-testid="firstName"
+              />
+            </Stack>
+            <Stack spacing={0}>
+              <Text><Text span color={colors.red}>*</Text> Last name</Text>
+              <InputLabel
+                type="text"
+                withAsterisk
+                {...form.getInputProps('lastname')}
+                placeholder="Last name"
+                data-testid="lastName"
+              />
+            </Stack>
             <InputLabel
-              autoFocus
-              type="text"
-              withAsterisk
-              {...form.getInputProps('firstname')}
-              placeholder="First name"
-              label="First name(s)"
-              isWhite={false}
-              data-testid="firstName"
-            />
-
-            <InputLabel
-              autoFocus
-              type="text"
-              withAsterisk
-              {...form.getInputProps('lastname')}
-              placeholder="Last name"
-              label="Last name"
-              isWhite={false}
-              data-testid="lastName"
-            />
-            <InputLabel
+              disabled
               readOnly
-              autoFocus
-              type="email"
-              withAsterisk
               {...form.getInputProps('email')}
               value={email}
-              placeholder="Your email address"
-              label="Your email address"
-              isWhite={false}
+              label="Email address"
             />
-            <Text>
-              We will send you a unique code via email to complete the login.
-            </Text>
-            <Group>
-              <Flex mih={50} align="flex-start" direction="row" wrap="wrap">
-                <Checkbox
-                  label="I agree to receive personalised marketing emails."
-                  {...form.getInputProps('marketingConsent', {
-                    type: 'checkbox',
-                  })}
-                  styles={{
-                    label: {
-                      order: -2,
-                      color: '#000000',
-                    },
-                  }}
-                  data-testid="marketingCheckbox"
-                />
-              </Flex>
-            </Group>
-            <Group>
-              <Button fullWidth type="submit" data-testid="loginAfterSignUp">
-                Login
-              </Button>
-            </Group>
+            <Checkbox
+              label="I agree to receive personalised marketing emails."
+              {...form.getInputProps('marketingConsent', {
+                type: 'checkbox',
+              })}
+              styles={{
+                input: {
+                  borderWidth: 2,
+                },
+              }}
+              data-testid="marketingCheckbox"
+            />
+            <Button fullWidth type="submit" data-testid="loginAfterSignUp">
+              Login
+            </Button>
           </Stack>
         </Stack>
       </form>
