300538a50ba17ed3e39e1db41e9dd51d6e55b054
Dimitar Danailov <dimityr.danailov@gmail.com>
2023-09-04 11:57:25 +0300

    feat:baas93 - Placeholder app - tech task. URL improvements.

--- apps/placeholder-app/src/app/(home)/urls.tsx
+++ apps/placeholder-app/src/app/(home)/urls.tsx
@@ -1,7 +1,7 @@
 const urls = [
   {
-    value: 'https://partner-local.test.cergea.com:4011',
-    label: 'Local: https://partner-local.test.cergea.com:4011',
+    value: 'https://booking-local.test.cergea.com:4011',
+    label: 'Local: https://booking-local.test.cergea.com:4011',
   },
   {
     value: 'https://booking.test.cergea.com',
