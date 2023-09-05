4f80c1067562ed3def526fc8e715772ca0f5a48d
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-09-05 15:29:10 +0300

    feat: updates for slots data wip

--- apps/booking/components/flightInfo/FlightData.tsx
+++ apps/booking/components/flightInfo/FlightData.tsx
@@ -1,5 +1,5 @@
 import { FlightDetails } from '@collinsonx/utils';
-import { Accordion, Grid } from '@collinsonx/design-system/core';
+import { Accordion } from '@collinsonx/design-system/core';
 
 export interface DepartureFlightInfo {
   airport: { iata: string };
@@ -20,46 +20,36 @@ const FlightData = ({ flightInfoData }: FlightDataProps) => {
   return (
     <>
       {flightInfoData && flightInfoData[0] ? (
-        <Grid>
-          <Grid.Col sm="auto" md="auto" lg={3}>
-            <Accordion variant="separated">
-              <Accordion.Item value="customization">
-                <Accordion.Control>
-                  Departing Flight Information
-                </Accordion.Control>
-                <Accordion.Panel>
-                  <p>
-                    Departing Airport: {flightInfoData[0].departure?.airport}
-                  </p>
-                  <p>
-                    Departing Date and Time (local):{' '}
-                    {flightInfoData[0].departure?.dateTime?.local}
-                  </p>
-                  <p>
-                    Departing Date and Time (utc):{' '}
-                    {flightInfoData[0].departure?.dateTime?.utc}
-                  </p>
-                </Accordion.Panel>
-              </Accordion.Item>
-              <Accordion.Item value="flexibility">
-                <Accordion.Control>
-                  Arrival Flight Information
-                </Accordion.Control>
-                <Accordion.Panel>
-                  <p>Arrival Airport: {flightInfoData[0].arrival?.airport}</p>
-                  <p>
-                    Arrival Date and Time (local):{' '}
-                    {flightInfoData[0].arrival?.dateTime?.local}
-                  </p>
-                  <p>
-                    Arrival Date and Time (utc):{' '}
-                    {flightInfoData[0].arrival?.dateTime?.utc}
-                  </p>
-                </Accordion.Panel>
-              </Accordion.Item>
-            </Accordion>
-          </Grid.Col>
-        </Grid>
+        <Accordion variant="separated">
+          <Accordion.Item value="customization">
+            <Accordion.Control>Departing Flight Information</Accordion.Control>
+            <Accordion.Panel>
+              <p>Departing Airport: {flightInfoData[0].departure?.airport}</p>
+              <p>
+                Departing Date and Time (local):{' '}
+                {flightInfoData[0].departure?.dateTime?.local}
+              </p>
+              <p>
+                Departing Date and Time (utc):{' '}
+                {flightInfoData[0].departure?.dateTime?.utc}
+              </p>
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value="flexibility">
+            <Accordion.Control>Arrival Flight Information</Accordion.Control>
+            <Accordion.Panel>
+              <p>Arrival Airport: {flightInfoData[0].arrival?.airport}</p>
+              <p>
+                Arrival Date and Time (local):{' '}
+                {flightInfoData[0].arrival?.dateTime?.local}
+              </p>
+              <p>
+                Arrival Date and Time (utc):{' '}
+                {flightInfoData[0].arrival?.dateTime?.utc}
+              </p>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
       ) : (
         ''
       )}
--- apps/booking/pages/booking.tsx
+++ apps/booking/pages/booking.tsx
@@ -20,12 +20,14 @@ import GuestInfo from '@components/GuestInfo';
 import usePayload from 'hooks/payload';
 import { BookingGuests, ViewStep } from 'types/booking';
 import { ArrowLeft } from '@collinsonx/design-system/assets/icons';
-import { FlightDetails } from '@collinsonx/utils';
-import { getFlightDetails } from '@collinsonx/utils/queries';
+import { Availability, FlightDetails } from '@collinsonx/utils';
+import { getAvailableSlots, getFlightDetails } from '@collinsonx/utils/queries';
 import {
   AIRPORT_CODE_TYPE,
   DATE_FORMAT,
+  LOUNGE,
   OAG_API_VERSION,
+  TRAVEL_TYPE,
 } from 'config/Constants';
 
 import { formatDate } from '../utils/DateFormatter';
@@ -34,6 +36,7 @@ import { useLazyQuery, useQuery } from '@collinsonx/utils/apollo';
 import { validateFlightNumber } from '../utils/flightValidation';
 import dayjs from 'dayjs';
 import FlightData from '@components/flightInfo/FlightData';
+import AvailableSlots from '@components/flightInfo/AvailableSlots';
 
 interface DepartureFlightInfo {
   airport: { iata: string };
@@ -105,13 +108,44 @@ const Lounge = () => {
     pollInterval: 300000,
     fetchPolicy: 'network-only',
     notifyOnNetworkStatusChange: true,
-    onCompleted: (data) => {
-      /*setFlightData(data?.getFlightDetails[0]);
-      onSuccess(data?.getFlightDetails[0]);*/
-    },
   });
 
-  console.log(date);
+  const {
+    loading: slotsLoading,
+    error: slotsError,
+    data: slotsData,
+  } = useQuery<{
+    getAvailableSlots: Availability;
+  }>(getAvailableSlots, {
+    variables: {
+      data: {
+        flightInformation: {
+          type: TRAVEL_TYPE,
+          dateTime:
+            flightInfoData?.getFlightDetails[0].departure?.dateTime?.local,
+          airport: flightInfoData?.getFlightDetails[0].departure?.airport,
+          terminal: '-1',
+        },
+        guests: {
+          adultCount: guests.adults,
+          childrenCount: guests.children,
+          seniorCount: guests.seniors,
+          infantCount: guests.infants,
+        },
+        product: {
+          productType: LOUNGE,
+          productID: '1139',
+          supplierCode: '123',
+        },
+      },
+    },
+    skip: !flightInfoData,
+    pollInterval: 300000,
+    fetchPolicy: 'network-only',
+    notifyOnNetworkStatusChange: true,
+  });
+
+  console.log(slotsData);
 
   const handleClickCheckAvailability = () => {
     fetchFlightInfo();
@@ -136,7 +170,7 @@ const Lounge = () => {
         <Flex justify="center" align="center">
           <Stack maw={591} spacing={24}>
             <LoungeInfo lounge={lounge} loading={!lounge} />
-            {!flightInfoData ? (
+            {!flightInfoData && !slotsData ? (
               <FlightInfo
                 step={step}
                 date={date}
@@ -145,9 +179,14 @@ const Lounge = () => {
                 flightNumber={flightNumber}
                 onChangeFlightNumber={setFlightNumber}
               />
-            ) : (
+            ) : null}
+
+            {flightInfoData ? (
               <FlightData flightInfoData={flightInfoData?.getFlightDetails} />
-            )}
+            ) : null}
+            {slotsData ? (
+              <AvailableSlots availableSlots={slotsData.getAvailableSlots} />
+            ) : null}
             <Box sx={{ borderBottom: '1px solid  #C8C9CA' }} />
             <GuestInfo
               step={step}
