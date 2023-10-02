6f8f7612319fbba837234d13ab3d00a9fa27375d
nikitas-fp <123029993+nikitas-fp@users.noreply.github.com>
2023-10-02 14:09:02 +0300

    chore: merge (#639)

--- apps/booking/pages/api/graphql.ts
+++ apps/booking/pages/api/graphql.ts
@@ -16,6 +16,7 @@ export const config = {
 
 const handler = (req: NextApiRequest, res: ServerResponse) => {
   return new Promise((resolve, reject) => {
+    req.url = '';
     proxy.web(
       req,
       res,
