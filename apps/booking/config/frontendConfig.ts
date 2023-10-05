226a037b8fb9d7f221e3eedc4f71c667073c28d9
Ash Heskes <me@ashheskes.com>
2023-10-05 09:59:38 +0100

    feat: send `accountProvider` when logging in (#655)
    + Add support for sending arbitrary data from the `userContext` to all
    api calls.

--- apps/booking/config/frontendConfig.ts
+++ apps/booking/config/frontendConfig.ts
@@ -5,13 +5,26 @@ import Session, { InputType } from 'supertokens-auth-react/recipe/session';
 
 const sessionTokenFrontendDomain = process.env.NEXT_PUBLIC_SESSION_SCOPE;
 
+async function userContextHandler(context: any) {
+  const { userContext = {}, requestInit } = context;
+  const body = requestInit.body ? JSON.parse(requestInit.body) : {};
+
+  requestInit.body = JSON.stringify({ ...body, userContext });
+
+  return context;
+}
+
 export const frontendConfig = () => {
   return {
     appInfo,
     recipeList: [
-      Session.init({ sessionTokenFrontendDomain } as InputType),
+      Session.init({
+        sessionTokenFrontendDomain,
+        preAPIHook: userContextHandler,
+      } as InputType),
       ThirdPartyPasswordless.init({
         contactMethod: 'EMAIL',
+        preAPIHook: userContextHandler,
       }),
     ],
   };
