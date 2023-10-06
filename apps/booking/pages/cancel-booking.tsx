c3eb6a42949115fb2df5f3efed1d5908f7ccff29
Nikitas Frantzeskakis <nikitas.frantzeskakis@foolproof.co.uk>
2023-10-06 19:26:15 +0300

    feat: apply FE rules from BAAS-464

--- apps/booking/pages/cancel-booking.tsx
+++ apps/booking/pages/cancel-booking.tsx
@@ -23,7 +23,32 @@ import { InfoPanel } from 'utils/PanelInfo';
 import { LoungeInfo } from '@components/LoungeInfo';
 import BackToLounge from '@components/BackToLounge';
 
+import { BookingError } from '../constants';
+
+const {
+  ERR_BOOKING_NOT_FOUND,
+  ERR_BOOKING_ALREADY_CANCELLED,
+  ERR_BOOKING_NOT_OWNED,
+  ERR_CANCELLATION_FAILED,
+  ERR_CANCELATION_NOT_ALLOWED,
+  ERR_SOMETHING_WENT_WRONG,
+  ERR_CANCELLATION_FAILED_WITH_SUCCESS,
+} = BookingError;
+
 const { bookingId } = BookingQueryParams;
+
+const cancellationMessages: Record<string, string> = {
+  [ERR_BOOKING_NOT_FOUND]: 'The booking cannot be found',
+  [ERR_BOOKING_ALREADY_CANCELLED]: 'The booking has been already cancelled',
+  [ERR_BOOKING_NOT_OWNED]:
+    'Sorry, something went wrong with your booking, please try again later or contact support',
+  [ERR_CANCELLATION_FAILED]:
+    'Sorry, something went wrong with your booking, please try again later or contact support',
+  [ERR_CANCELATION_NOT_ALLOWED]:
+    "We're sorry, this booking cannot be cancelled within 48 hours of booking arrival time.",
+  [ERR_SOMETHING_WENT_WRONG]:
+    'Sorry, something went wrong with your booking, please try again later or contact support',
+};
 
 export default function CancelBooking() {
   const router = useRouter();
@@ -33,6 +58,7 @@ export default function CancelBooking() {
   const [createLoading] = useState(false);
   const [opened, { open, close }] = useDisclosure(false);
   const [dateError, setDateError] = useState<Boolean>(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const { data: bookingDetails } = useQuery<{
     getBookingByID: Booking;
@@ -48,6 +74,8 @@ export default function CancelBooking() {
     useMutation(cancelBooking);
 
   const handleCancellation = () => {
+    close();
+    setErrorMessage('');
     if (
       bookingDetails &&
       cancellationDateValidation(
@@ -56,12 +84,25 @@ export default function CancelBooking() {
     ) {
       mutate({
         variables: { cancelBookingId: emailBookingId },
-        onCompleted(data) {
+      }).then((response) => {
+        if (response && response.errors) {
+          const item = response.errors[0];
+          const code = item?.extensions?.code;
+          const message = cancellationMessages[code as string];
+          if (message) {
+            setErrorMessage(message);
+          } else if (code === ERR_CANCELLATION_FAILED_WITH_SUCCESS) {
+            router.push({
+              pathname: '/cancelled-booking-confirmation',
+              query: { id: emailBookingId },
+            });
+          }
+        } else {
           router.push({
             pathname: '/cancelled-booking-confirmation',
             query: { id: emailBookingId },
           });
-        },
+        }
       });
     } else {
       setDateError(true);
@@ -163,6 +204,9 @@ export default function CancelBooking() {
                 >
                   {
                     <Box sx={{ width: '100%' }}>
+                      {errorMessage && (
+                        <Notification>{errorMessage}</Notification>
+                      )}
                       {bookingDetails?.getBookingByID?.experience && (
                         <Stack spacing={8} sx={{ padding: '20px' }}>
                           <Heading as="h2" margin={0} padding={0}>
