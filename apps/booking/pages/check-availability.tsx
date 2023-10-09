9b64807523535616ea54c8c814b9917ef8d9a9ae
arismarkoFP <122892172+arismarkoFP@users.noreply.github.com>
2023-10-09 15:24:13 +0100

    Fix/baas 506 styling fixes (#724)
    * fix: added fix for the login button
    
    * fix: fixes in styles

--- apps/booking/pages/check-availability.tsx
+++ apps/booking/pages/check-availability.tsx
@@ -409,7 +409,7 @@ export default function ConfirmAvailability() {
                           },
                         }}
                       >
-                        <EditableTitle title="Who's coming" as="h2">
+                        <EditableTitle title="Who's coming?" as="h2">
                           <GuestCount
                             adults={adults}
                             children={children}
