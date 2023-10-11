5f222a89e2b30cea42d0c110e2f2ba915fc8fa39
asifhafeez89 <134387362+asifhafeez89@users.noreply.github.com>
2023-10-11 19:56:03 +0100

    Test/baas 501 e2e tests (#760)
    * test: added all initial redirect tests - 4 tests failing - skipping these
    
    * test: added onboarding test cases
    
    * test: added create booking, initial redirect and onboarding tests with page object model
    
    * test: updated all tests
    
    * test: update runners to 10 and add await for locator
    
    * test: increase timeout and number of parallel users
    
    * test: add caching and run with less workers
    
    * test: debug failing step in test
    
    * chore: remove forbid only
    
    * chore: added wait for page to be idle
    
    * chore: run all tests
    
    * chore: refactored tests

--- packages/collinsonx-tests-e2e/tests/booking/pages/PreBookPage.js
+++ packages/collinsonx-tests-e2e/tests/booking/pages/PreBookPage.js
@@ -4,7 +4,83 @@ class PreBookPage {
   }
 
   async loungeTitle() {
-    return await this.page.getByTestId('loungeName').innerText();
+    await this.page.waitForTimeout(1000);
+    const loungeNameElement = await this.page.waitForSelector(
+      '[data-testid="loungeName"]'
+    );
+    const loungeNameText = await loungeNameElement.innerText();
+    return loungeNameText;
+  }
+
+  async airportAndTerminalElement(airportAndTerminal) {
+    const airportAndTerminalSelector = `text=${airportAndTerminal}`;
+    return await this.page.$(airportAndTerminalSelector);
+  }
+
+  async priceElement(price) {
+    const priceSelector = `text=${price}`;
+    return await this.page.$(priceSelector);
+  }
+
+  async openDatePicker() {
+    const datePicker = await this.page.waitForSelector(
+      '.mantine-DatePickerInput-input'
+    );
+    await datePicker.click();
+  }
+
+  async clickNextMonth() {
+    const nextMonth = await this.page.waitForSelector('[data-next="true"]');
+    await nextMonth.click();
+  }
+
+  async selectDate(dateString) {
+    const dateSelector = `[aria-label="${dateString}"]`;
+    const date = await this.page.waitForSelector(dateSelector);
+    await date.click();
+    await this.page.waitForTimeout(500);
+  }
+
+  async inputFlightNumber(flightNumber) {
+    await this.page.waitForLoadState('networkidle');
+    const flightNumberInput = await this.page.waitForSelector(
+      '[placeholder="E.g. EZY123"]'
+    );
+    await flightNumberInput.type(flightNumber, { delay: 200 });
+  }
+
+  async increaseAdultGuests() {
+    const increaseAdultsButtonSelector = 'button:has-text("+")';
+    const increaseAdultsButton = await this.page.$(
+      increaseAdultsButtonSelector
+    );
+    await increaseAdultsButton.click();
+  }
+
+  async clickSubmit() {
+    const submitButtonSelector = 'button[type="submit"]';
+    const submitButton = await this.page.$(submitButtonSelector);
+    await submitButton.click();
+  }
+
+  async invalidFlightError() {
+    const errorText =
+      'Flight details not recognised. Please check and try again.';
+    const errorSelector = `text=${errorText}`;
+    return await this.page.waitForSelector(errorSelector);
+  }
+
+  async flightDateError() {
+    const errorText = 'Must provide date of flight.';
+    const errorSelector = `text=${errorText}`;
+
+    return await this.page.waitForSelector(errorSelector);
+  }
+
+  async airportMismatchWarning() {
+    const warningText = `Airports don't match`;
+    const warningSelector = `text=${warningText}`;
+    return await this.page.$(warningSelector);
   }
 }
 
