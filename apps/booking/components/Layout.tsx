60320874e9ee95e4a49c5cba2e2173094ad05164
arismarkoFP <122892172+arismarkoFP@users.noreply.github.com>
2023-10-20 14:39:46 +0100

    Fix/bass 123 conficlts changes (#859)
    * feat: logo resize
    
    * fix: added hsbc changes
    
    * fix: eslint back to what it was
    
    * fix: added eslint the way it was
    
    * fix: added eslint the way it was
    
    * fix: added all changes requested
    
    * fix: fixed build
    
    * fix: added correct code
    
    * fix: added chanegs that were removed
    
    * fix: added fixes
    
    ---------
    
    Co-authored-by: Panos Voudouris <panos.voudouris@foolproof.co.uk>
    Co-authored-by: Dimitar Danailov (a.k.a Mitco) <dimityr.danailov@gmail.com>

--- apps/booking/components/Layout.tsx
+++ apps/booking/components/Layout.tsx
@@ -29,22 +29,11 @@ const Layout = forwardRef(({ children }: LayoutProps, ref: ContainerRef) => {
       ref={ref}
     >
       <Box
-        mb={2}
-        mt={2}
         sx={{
           width: '100%',
           backgroundColor: colors.white,
-          boxShadow: `4px 4px 4px 4px ${colors.shadow}`,
-          position: 'relative',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignContent: 'center',
-          alignItems: 'center',
-          height: '100px',
-          '@media (max-width: 768px)': {
-            height: '50px',
-          },
+          boxShadow: `4px 4px 4px 0px ${colors.shadow}`,
+          position: 'fixed',
           zIndex: 200,
         }}
       >
