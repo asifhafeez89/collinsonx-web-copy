a7d6e09f421d1742e4fdce0d2f1e45905700e02b
arismarkoFP <122892172+arismarkoFP@users.noreply.github.com>
2023-10-20 13:15:53 +0100

    Feat/baas 123 logo resize (#851)
    * feat: logo resize
    
    * fix: added hsbc changes
    
    * fix: eslint back to what it was
    
    * fix: added eslint the way it was
    
    * fix: added eslint the way it was
    
    * fix: added all changes requested
    
    * fix: fixed build
    
    * fix: added correct code
    
    ---------
    
    Co-authored-by: Panos Voudouris <panos.voudouris@foolproof.co.uk>

--- apps/booking/components/AppLogo.tsx
+++ apps/booking/components/AppLogo.tsx
@@ -4,94 +4,50 @@ import {
   LogoLK,
   LogoPP,
 } from '@collinsonx/design-system/assets/logo';
-import { Box, MediaQuery } from '@mantine/core';
+import { Box } from '@mantine/core';
 import React, { ReactNode } from 'react';
 
 const { LK, PP } = AccountProvider;
 const { Mastercard_HSBC } = Client;
 
-interface ShowDesktopInterface {
-  children: React.ReactNode;
-}
-
-const ShowMobile = ({ children }: ShowDesktopInterface) => {
-  return (
-    <Box
-      sx={{
-        '@media (min-width: 768px)': {
-          display: 'none',
-        },
-
-        '@media (max-width: 767px)': {
-          display: 'block',
-        },
-      }}
-    >
-      {children}
-    </Box>
-  );
-};
-
-const ShowDesktop = ({ children }: ShowDesktopInterface) => {
-  return (
-    <Box
-      sx={{
-        '@media (max-width: 768px)': {
-          display: 'none',
-        },
-
-        '@media (min-width: 767px)': {
-          display: 'block',
-        },
-      }}
-    >
-      {children}
-    </Box>
-  );
-};
-
-const size = {
-  desktop: {
-    width: '28em',
-    height: '5em',
-  },
-  mobile: {
-    width: '14em',
-    height: '3em',
-  },
-};
-
 const logos: Record<AccountProvider | string, ReactNode> = {
   [LK]: (
-    <>
-      <ShowDesktop>
-        <LogoLK width={size.desktop.width} height={size.desktop.height} />
-      </ShowDesktop>
-      <ShowMobile>
-        <LogoLK height={size.mobile.height} />
-      </ShowMobile>
-    </>
+    <Box
+      sx={{
+        transform: 'scale(0.35)',
+        '@media (max-width: 768px)': {
+          transform: 'scale(0.17)',
+        },
+      }}
+    >
+      <LogoLK />
+    </Box>
   ),
 
   [PP]: (
-    <>
-      <ShowDesktop>
-        <LogoPP width={size.desktop.width} height={size.desktop.height} />
-      </ShowDesktop>
-      <ShowMobile>
-        <LogoPP height={size.mobile.height} />
-      </ShowMobile>
-    </>
+    <Box
+      sx={{
+        transform: 'scale(0.36)',
+        '@media (max-width: 768px)': {
+          transform: 'scale(0.18)',
+        },
+      }}
+    >
+      <LogoPP />
+    </Box>
   ),
   [Mastercard_HSBC]: (
-    <>
-      <ShowDesktop>
-        <LogoHSBC width={size.desktop.width} height={size.desktop.height} />
-      </ShowDesktop>
-      <ShowMobile>
-        <LogoHSBC height={size.mobile.height} />
-      </ShowMobile>
-    </>
+    <Box
+      sx={{
+        marginTop: '10px',
+        transform: 'scale(0.70)',
+        '@media (max-width: 768px)': {
+          transform: 'scale(0.45)',
+        },
+      }}
+    >
+      <LogoHSBC />
+    </Box>
   ),
 };
 
