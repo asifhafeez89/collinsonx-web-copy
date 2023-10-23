99e4c8cefa8e1068d4eb8f1166262bf9b9980ee8
Dimitar Danailov (a.k.a Mitco) <dimityr.danailov@gmail.com>
2023-10-23 11:33:24 +0300

    feat: source code improvements. environment.d.ts adds autocomplete (#788)
    * feat: source code improvements. environment.d.ts adds autocomplete
    
    * feat: front-end technical debt. environment improvements
    
    * feat: front-end technical debt. getValue was removed

--- apps/placeholder-app/environment.d.ts
+++ apps/placeholder-app/environment.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    readonly NEXT_PUBLIC_JWT_SECRET_KEY: string;
+    readonly NEXT_PUBLIC_JWT_SECRET_KEY_TEST: string;
+    readonly NEXT_PUBLIC_JWT_SECRET_KEY_UAT: string;
+    readonly NEXT_PUBLIC_JWT_SECRET_KEY_PROD: string;
   }
 }
